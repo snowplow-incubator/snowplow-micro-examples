df909b4f81cbac18f407a9a07e75dc7809a69b7b
Emily Youngs <emilyyoungs@pop-os.localdomain>
2020-07-09 17:14:48 +0900

    Added cart function and add to cart and remove from cart

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -3,39 +3,44 @@ from django.db import models
 # Create your models here.
 
 class ViewProductItem:
-    def __init__(self, id, image_name, description, quantity, sizes=None, colours=None):
+    def __init__(self, id, image_name, description, quantity, colours, price, currency):
         self.id = id
         self.image_name = image_name
         self.description = description
         self.quantity = range(quantity + 1)
-        self.sizes = sizes
         self.colours = colours
+        self.price = price
+        self.currency = currency
 
 productDB = [
         {"id": "1",
         "image_name": "hat.jpg",
          "description": "One-size summer hat",
          "quantity": 4,
-         "sizes": ["oneSize"],
-         "colours":["Black", "White"]},
+         "colours":["Black", "White"],
+         "price": 15.50,
+         "currency": "GBP"},
         {"id" : "2",
-        "image_name": "jeans.jpg",
-         "description": "Skinny-fit denim jeans",
+        "image_name": "green_hat.jpg",
+         "description": "One-size bucket hat",
          "quantity": 4,
-         "sizes": ["6","8","10","12","14","16"],
-         "colours":["Black", "Blue"]},
+         "colours":["Green", "Blue", "Yellow"],
+         "price": 24.49,
+         "currency": "GBP"},
         {"id":"3",
-        "image_name": "tshirt.jpg",
-         "description": "Casual Basic Tee",
+        "image_name": "bag.jpg",
+         "description": "Plain tote bag ",
          "quantity": 4,
-         "sizes": ["6","8","10","12","14","16"],
-         "colours":["Black", "White", "Pink"]}
+         "colours":["Black", "White"],
+         "price": 20.50,
+         "currency": "GBP"}
     ]
 
 def retrieveProducts():
     product_items = []
     for db_item in productDB:
-        product_items.append(ViewProductItem(db_item["id"], db_item["image_name"], db_item["description"], db_item["quantity"], db_item["sizes"], db_item["colours"]))
+        product_items.append(ViewProductItem(db_item["id"], db_item["image_name"], db_item["description"],
+         db_item["quantity"], db_item["colours"], db_item["price"], db_item["currency"]))
     return product_items
 
 
