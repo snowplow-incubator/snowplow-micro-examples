0520da899b29832c96f6c328577fcf48ad14beb7
Emily Youngs <emilyyoungs@pop-os.localdomain>
2020-07-10 16:00:02 +0900

    Fixed add to cart functions

--- nightwatch/ecommerceTests.js
+++ nightwatch/ecommerceTests.js
@@ -4,45 +4,45 @@ module.exports = {
           .resetMini()
       },
 
-    'Number of good events after CLICK is equal to two' : function(browser) {
+    'Number of good events after ADDTOBASKET is equal to two' : function(browser) {
         browser
             .url('http://127.0.0.1:8001/');
 
         const imgClass = '.floated_img[type="image"]';
 
-        browser.waitForElementVisible(imgClass, 'Image loaded successfully')
+        browser.waitForElementVisible(imgClass, 'Button loaded successfully')
             .click(imgClass, function (result) {
-                this.assert.equal(true, result.status == 0, "Image clicked successfully");
+                this.assert.equal(true, result.status == 0, "Button clicked successfully");
             })
 
         browser.assert.noOfGoodEvents(3);
         browser.assert.noBadEvents();
         browser.assert.noOfTotalEvents(3);
-        browser.assert.trackerSuccess(["Click", "Hover"]);
+        browser.assert.trackerSuccess(["Selection", "AddToBasket"]);
 
 
     },
 
-    'Number of good events after HOVER is equal to one' : function(browser) {
+    'Number of good events after SELECTION is equal to one' : function(browser) {
         browser
             .url('http://127.0.0.1:8001/');
 
         const imgGrid = "#image-grid";
         const imgClass = '.floated_img[type="image"]';
 
-        browser.waitForElementVisible(imgClass, 'Image loaded successfully')
+        browser.waitForElementVisible(imgClass, 'Selection loaded successfully')
             .moveToElement(imgClass,10,10);
         browser.moveToElement(imgGrid,400,400);
 
         browser.assert.noOfGoodEvents(2);
         browser.assert.noBadEvents();
         browser.assert.noOfTotalEvents(2);
-        browser.assert.trackerSuccess(["Hover"]);
+        browser.assert.trackerSuccess(["Selection", "AddToBasket"]);
 
 
     },
 
-    'Check proper values are sent for the CLICK event': function(browser) {
+    'Check proper values are sent for the SELECTION event': function(browser) {
         browser
             .url('http://127.0.0.1:8001/');
 
@@ -52,7 +52,7 @@ module.exports = {
         browser.waitForElementVisible(imgClass, 'Image loaded successfully')
             .click(imgClass);
 
-        browser.assert.trackerSuccess(["Click", "Hover"]);
+        browser.assert.trackerSuccess(["Selection"]);
         browser.assert.successfulEvent({
                 "eventType":"se",
                 "parameters":{
@@ -73,7 +73,7 @@ module.exports = {
 
     },
 
-    'Check proper values are sent for the HOVER event': function(browser) {
+    'Check proper values are sent for the ADD TO BASKET event': function(browser) {
 
         browser
             .url('http://127.0.0.1:8001/');
@@ -103,66 +103,6 @@ module.exports = {
 
                               }]
         })
-        browser.assert.trackerSuccess(["Hover"]);
+        browser.assert.trackerSuccess(["AddTOBasket"]);
 
-        },
-
-
-        'Check the order of events; hover always before click': function(browser) {
-
-                browser
-                    .url('http://127.0.0.1:8001/');
-                const imgGrid = "#image-grid";
-                const imgClass = '.floated_img[type="image"]';
-
-                 browser.waitForElementVisible(imgClass, 'Image loaded successfully')
-                    .moveToElement(imgClass,500,500);
-                browser.moveToElement(imgGrid,10,10);
-
-                browser.waitForElementVisible(imgClass, 'Image loaded successfully')
-                    .click(imgClass);
-
-                 browser.waitForElementVisible(imgClass, 'Image loaded successfully')
-                    .moveToElement(imgClass,10,10);
-                browser.moveToElement(imgGrid,400,400);
-
-                events_list = [
-                 {
-                "eventType":"se",
-                "parameters":{
-                    "se_ca" : "Faces",
-                    "se_ac" : "Hover",
-                    "se_pr" : 0,
-                    "se_va": 0
-                },
-                "contexts":[{
-                           schema: 'iglu:com.emily.test/user_context/jsonschema/1-0-0',
-                                data: {
-                                    id: 1234,
-                                    age: 25
-                                    }
-
-                                      }]
-                },
-                {
-                   "eventType":"se",
-                   "parameters":{
-                       "se_ca" : "Faces",
-                       "se_ac" : "Click",
-                       "se_pr" : 0,
-                       "se_va": "125,125"
-                   },
-                   "contexts":[{
-                                  schema: 'iglu:com.emily.test/user_context/jsonschema/1-0-0',
-                                       data: {
-                                           id: 1234,
-                                           age: 25
-                                           }
-
-                                         }]
-                   }]
-                browser.assert.orderOfEvents(events_list);
-                }
-
-
-};
+        }
